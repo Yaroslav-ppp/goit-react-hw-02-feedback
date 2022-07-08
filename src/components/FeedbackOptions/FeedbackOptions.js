c0e23351408c5610ba0958281881ba705553fda3
Yaroslav-ppp <83807204+Yaroslav-ppp@users.noreply.github.com>
2022-07-08 22:11:03 +0300

    marking

---
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,11 @@
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <ul>
+      {options.map(option => (
+        <li key={option}>
+          <button onClick={() => onLeaveFeedback(option)}>{option}</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,18 @@
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
+  return (
+    <>
+      <span>Good: {good}</span>
+      <span>Neutral: {neutral}</span>
+      <span>Bad: {bad}</span>
+
+      <span>Total: {total}</span>
+      <span>Positive feedback: {positivePercentage}%</span>
+    </>
+  );
+};
